9119ab4146d5ed4112b7f1b51e917d71cd70823e
Yu Hao <jinyu121@gmail.com>
2018-07-03 12:39:59 +0800

    Scripts again and again...

--- scripts/coco_to_voc_pure.py
+++ scripts/coco_to_voc_pure.py
@@ -4,13 +4,17 @@ import os
 from tqdm import tqdm
 from xmltodict import unparse
 
-NUM_GROUPS = 4
-START_FROM_1 = False
+# BBOX_OFFSET: Switch between 0-based and 1-based bbox
+# The COCO dataset is in 0-based format, while the VOC is 1-based
+# To keep 0-based, set it to 0. To make 1-based annotation, set it to 1.
+BBOX_OFFSET = 1
 
-dataset_base = os.path.join("data", "VOCdevkitCOCO", "VOCCOCO")
-dataset_dirs = {x: os.path.join(dataset_base, x) for x in ["Annotations", "ImageSets", "JPEGImages"]}
-dataset_dirs['ImageSets'] = os.path.join(dataset_dirs['ImageSets'], "Main")
-for k, d in dataset_dirs.items():
+src_base = os.path.join("data", "coco", "annotations")
+dst_base = os.path.join("data", "VOCdevkitCOCO", "VOCCOCO")
+
+dst_dirs = {x: os.path.join(dst_base, x) for x in ["Annotations", "ImageSets", "JPEGImages"]}
+dst_dirs['ImageSets'] = os.path.join(dst_dirs['ImageSets'], "Main")
+for k, d in dst_dirs.items():
     os.makedirs(d, exist_ok=True)
 
 
@@ -26,22 +30,17 @@ def base_dict(filename, width, height, depth=3):
     }
 
 
-def base_object(size_info, name, bbox, format="xyxy"):
-    assert format in ['xyxy', 'xywh'], "Unknow format"
-    if 'xyxy' == format:
-        x1, y1, x2, y2 = bbox
-    else:
-        x1, y1, w, h = bbox
-        x2, y2 = x1 + w, y1 + h
+def base_object(size_info, name, bbox):
+    x1, y1, w, h = bbox
+    x2, y2 = x1 + w, y1 + h
 
     width = size_info['width']
     height = size_info['height']
-    offset = 0 if START_FROM_1 else 1
 
-    x1 = max(x1, 1) - offset
-    y1 = max(y1, 1) - offset
-    x2 = min(x2, width) - offset
-    y2 = min(y2, height) - offset
+    x1 = max(x1, 1) + BBOX_OFFSET
+    y1 = max(y1, 1) + BBOX_OFFSET
+    x2 = min(x2, width) + BBOX_OFFSET
+    y2 = min(y2, height) + BBOX_OFFSET
 
     return {
         'name': name, 'pose': 'Unspecified', 'truncated': '0', 'difficult': '0',
@@ -49,7 +48,6 @@ def base_object(size_info, name, bbox, format="xyxy"):
     }
 
 
-src_base = os.path.join("data", "coco", "annotations")
 sets = {
     "trainval": os.path.join(src_base, "instances_train2017.json"),
     "test": os.path.join(src_base, "instances_val2017.json"),
@@ -67,17 +65,17 @@ for stage, filename in sets.items():
         images[im["id"]] = img
 
     for an in tqdm(data["annotations"], "Parse Annotations"):
-        ann = base_object(images[an['image_id']]['annotation']["size"],
-                          cate[an['category_id']], an['bbox'], format="xywh")
+        ann = base_object(images[an['image_id']]['annotation']["size"], cate[an['category_id']], an['bbox'])
         images[an['image_id']]['annotation']['object'].append(ann)
 
     for k, im in tqdm(images.items(), "Write Annotations"):
+        im['annotation']['object'] = im['annotation']['object'] or [None]
         unparse(im,
-                open(os.path.join(dataset_dirs["Annotations"], "{}.xml".format(str(k).zfill(12))), "w"),
+                open(os.path.join(dst_dirs["Annotations"], "{}.xml".format(str(k).zfill(12))), "w"),
                 full_document=False, pretty=True)
 
     print("Write image sets")
-    with open(os.path.join(dataset_dirs["ImageSets"], "{}.txt".format(stage)), "w") as f:
+    with open(os.path.join(dst_dirs["ImageSets"], "{}.txt".format(stage)), "w") as f:
         f.writelines(list(map(lambda x: str(x).zfill(12) + "\n", images.keys())))
 
     print("OK")
--- scripts/select_pic_voc.py
+++ scripts/select_pic_voc.py
@@ -1,6 +1,6 @@
 import os
+from collections import Counter
 from pprint import pprint
-from random import random
 
 from easydict import EasyDict
 from xmltodict import parse
@@ -8,76 +8,70 @@ from xmltodict import parse
 cfg = EasyDict({
     "base_dir": os.path.join('data', 'VOCdevkit2007', 'VOC2007'),
     "nop_limit": 10000,
-    "imdb_train": "voc_2007_trainval",
-    "imdb_test": "voc_2007_test",
-    "max_pic_train": 5,
-    "max_pic_eval": 5,
-    "label_names": ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
-                    'bus', 'car', 'cat', 'chair', 'cow',
-                    'diningtable', 'dog', 'horse', 'motorbike', 'person',
-                    'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'],
-    "nb_groups": 4,
-    "factor": 0.
+    "imdb_tra": "trainval",
+    "imdb_val": "test",
+    "max_pic_tra": 20000,
+    "max_pic_val": 20000,
+    "label_names": [
+        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+        'bus', 'car', 'cat', 'chair', 'cow',
+        'diningtable', 'dog', 'horse', 'motorbike', 'person',
+        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+    ],
+    "nb_groups": 4
 })
+
+sets_name = ["tra", "val"]
 
 txt_base = os.path.join(cfg.base_dir, 'ImageSets', 'Main')
 ann_base = os.path.join(cfg.base_dir, 'Annotations')
 
-sets_name = ["tra", "val"]
-sets_full_name = {
-    "tra": "trainval",
-    "val": "test"
-}
 
-nop_limit = cfg.nop_limit  # Abandon after many `do nothing`s
-
-filename = {
-    "tra": "{}.{}".format(cfg.imdb_train.split('_')[-1], "txt"),
-    "val": "{}.{}".format(cfg.imdb_test.split('_')[-1], "txt")
-}
-
-max_pic = {
-    "tra": cfg.max_pic_train,
-    "val": cfg.max_pic_eval
-}
-
-total_file = {"tra": [], "val": []}
-
-txt = {k: [x.strip() for x in open(os.path.join(txt_base, filename[k]))] for k in sets_name}
-counter = {k: {x: 0 for x in cfg.label_names} for k in sets_name}
+def prepare(filename):
+    print("Prepare", filename)
+    ids = [x.strip() for x in open(os.path.join(txt_base, filename + ".txt"))]
+    mem = {}
+    for line in ids:
+        anno_path = os.path.join(ann_base, '{}.xml'.format(line))
+        anno = parse(open(anno_path).read())['annotation']
+        # Ignore the images who do not contain any objects
+        objs = anno.get('object', [])
+        if not objs:
+            print("No object:", line)
+            continue
+        objs = objs if isinstance(objs, list) else [objs]
+        obj_cls = [cfg.label_names.index(o['name']) for o in objs]
+        mem[line] = set(obj_cls)
+    print("Prepare", "OK", "Images", "{}/{}".format(len(mem), len(ids)))
+    return mem
 
 
-def sel(file_set, pic_max, cls_low, cls_hig, tol):
-    counter_batch = {k: 0 for k in cfg.label_names}
-    remain = set([k for k in cfg.label_names if cls_low <= cfg.label_names.index(k) < cls_hig])
+def sel(mem, pic_max, cls_low, cls_hig, nop_limit=1000):
+    counter_batch = Counter()
+    remain = set(list(range(cls_low, cls_hig)))
     result = []
     do_nothing = 0
-    while len(remain) or (len(result) < pic_max and do_nothing < nop_limit):
+    lines = list(mem.keys())
+
+    while len(remain) or (len(result) < pic_max and do_nothing < nop_limit and len(lines)):
         do_nothing += 1
-        line = file_set.pop(0)
+        line = lines.pop(0)
+        objs = mem[line]
 
-        anno_path = os.path.join(ann_base, '{}.xml'.format(line))
-        objs = parse(open(anno_path).read())['annotation']['object']
-        objs = objs if isinstance(objs, list) else [objs]
-        obj_cls = list(set([o['name'] for o in objs]))
-
-        for name in obj_cls:
-            if cfg.label_names.index(name) >= cls_hig:  # 1. Class range
-                file_set.append(line)
+        for item in objs:
+            if item >= cls_hig:  # 1. Class range
+                lines.append(line)
                 break
-            if counter_batch[name] > pic_max:  # 2. Number of images
-                file_set.append(line)
-                break
-            if cfg.label_names.index(name) < cls_low and random() > tol:  # 3, Leaky
-                file_set.append(line)
+            if counter_batch[item] > pic_max:  # 2. Number of images
+                lines.append(line)
                 break
         else:
             result.append(line)
-            for name in obj_cls:
-                counter_batch[name] += 1
-            remain.discard(name)
+            counter_batch.update(list(objs))
+            remain = remain - mem[line]
+            del mem[line]
             do_nothing = 0
-    return result, counter_batch, file_set
+    return result, counter_batch, mem
 
 
 def write_to_file(fnm, data):
@@ -85,25 +79,27 @@ def write_to_file(fnm, data):
         f.writelines(["{}\n".format(x) for x in data])
 
 
+data = {stage: prepare(cfg["imdb_{}".format(stage)]) for stage in sets_name}
+total_file = {stage: [] for stage in sets_name}
+
+counter = {stage: Counter() for stage in sets_name}
+
 for group in range(cfg.nb_groups):
     print('=' * 10, "Group", group, '=' * 10)
     sta = int(len(cfg.label_names) * 1. / cfg.nb_groups * group)
     fin = int(len(cfg.label_names) * 1. / cfg.nb_groups * (group + 1))
 
     for stage in sets_name:
-        print(stage.capitalize(), end="...")
-        files, counter_g, txt[stage] = sel(txt[stage], max_pic[stage], sta, fin, cfg.factor / (group + 1))
-        print("OK")
-
+        print(stage.capitalize())
+        # sta = 0 if "val" == stage else sta
+        files, counter_g, data[stage] = sel(data[stage], cfg["max_pic_{}".format(stage)], sta, fin, cfg.nop_limit)
         print(len(files), counter_g)
 
         total_file[stage] += files
-
-        for k, v in counter_g.items():
-            counter[stage][k] += v
-
-        write_to_file(os.path.join(txt_base, '{}Step{}.txt'.format(sets_full_name[stage], group)), files)
-        write_to_file(os.path.join(txt_base, '{}Step{}a.txt'.format(sets_full_name[stage], group)), total_file[stage])
+        counter[stage].update(counter_g)
+        write_to_file(os.path.join(txt_base, '{}Step{}.txt'.format(cfg["imdb_{}".format(stage)], group)), files)
+        write_to_file(os.path.join(txt_base, '{}Step{}a.txt'.format(cfg["imdb_{}".format(stage)], group)),
+                      total_file[stage])
 
 print('=' * 10, "Summary", '=' * 10)
 print("\t".join(["{}:{}".format(x, len(total_file[x])) for x in sets_name]))
